650b30df821412de10f17c85389a7e2a0a5f6106
Trojan <bonnie.alina2016@yandex.ru>
2024-12-04 15:17:50 +0300

    Remove unnecessary modules

--- index.js
+++
@@ -1,5 +0,0 @@
-const CC = require("currency-converter-lt");
-let currencyConverter = new CC({ from: "USD", to: "JPY", amount: 100 });
-currencyConverter.convert().then((response) => {
-  console.log(`Result: ${response}`);
-});
--- index2.js
+++
@@ -1,4 +0,0 @@
-const CC = require("currency-converter-lt");
-let currencyConverter = new CC({ from: "USD", to: "JPY", amount: 100 });
-
-console.log(`Result: ${response}`);
