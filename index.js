17c298aff67990bdcfa8e0c18321ee47c5e58305
Trojan <bonnie.alina2016@yandex.ru>
2024-12-04 15:49:02 +0300

    add math module and conclusion in the index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const os = require("os");
+/*const os = require("os");
 let res = os.platform();
-console.log(res);
+console.log(res);*/
+
+const my_math = require("./my_math");
+let rest = my_math.add(4, 5);
+console.log(rest);
