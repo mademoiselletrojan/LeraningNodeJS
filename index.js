c6bbc978e974d502576ca430455d60d505f53aa4
Trojan <bonnie.alina2016@yandex.ru>
2024-12-05 10:14:16 +0300

    add pages about and user

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("This is home page");
 });
+app.get("/about", (req, res) => {
+  res.send("This is about page");
+});
+app.get("/user/:username/:id", (req, res) => {
+  res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`);
+});
 
 const port = 3000;
 
