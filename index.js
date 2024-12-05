ee21f43bcfc36785095a2816955f3748c63e99c0
Trojan <bonnie.alina2016@yandex.ru>
2024-12-05 09:09:45 +0300

    adding server with conclusion

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require("http");
-let server = http.createServer((req, res) => {});
+
+let server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.end("Bonjour <b>mon ami</b>");
+});
 
 const Port = 3000;
 const Host = "127.0.0.1";
