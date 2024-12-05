fde09d84d862c1b586f96b9694d851eab0c68cc9
Trojan <bonnie.alina2016@yandex.ru>
2024-12-05 09:30:18 +0300

    conclusion html

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 let server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  res.end("Bonjour <b>mon ami</b>");
+
+  const filePath = path.join(__dirname, "temp", "index.html");
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(res);
+
+  stream.on("error", (err) => {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Internal Server Error");
+    console.error("Error reading file:", err);
+  });
 });
 
 const Port = 3000;
