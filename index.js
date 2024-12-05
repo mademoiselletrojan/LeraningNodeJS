b7258a29dffaa45986c4f7194983af6f3d0ce5d4
Trojan <bonnie.alina2016@yandex.ru>
2024-12-05 09:59:35 +0300

    learning hot to use express-module

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
 
-let server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-
-  if (req.url == "/") fs.createReadStream("./temp/index.html").pipe(res);
-  else if (req.url == "/about")
-    fs.createReadStream("./temp/about.html").pipe(res);
-  else fs.createReadStream("./temp/error.html").pipe(res);
+const app = express();
+app.get("/", (req, res) => {
+  res.send("This is home page");
 });
 
-const Port = 3000;
-const Host = "127.0.0.1";
+const port = 3000;
 
-server.listen(Port, Host, () => {
-  console.log(`Server has started: http://${Host}:${Port}`);
+app.listen(port, () => {
+  console.log(`Server started: http://localhost:${port}`);
 });
